f1a9cfa5d978c88f030ce7de5adfaba90196f23c
Sherman Devers <67293014+ShermanDevers@users.noreply.github.com>
2024-08-09 16:39:13 -0500

    Update init.lua to match current config

--- init.lua
+++ init.lua
@@ -5,19 +5,31 @@ local vim = vim
 
 vim.opt.clipboard = "unnamedplus"
 vim.opt.termguicolors = true
+vim.opt.autochdir = true
 
 local Plug = vim.fn["plug#"]
 
 vim.call("plug#begin")
-
 Plug("Mofiqul/vscode.nvim")
-Plug("mg979/vim-visual-multi")
 vim.call("plug#end")
 vim.cmd("colorscheme vscode")
+
+-- Custom Keymaps
 vim.keymap.set("n", "<C>h", "")
 vim.keymap.set("n", "<C>l", "")
 vim.keymap.set("n", "<F3>", "<cmd>Neotree toggle<cr>")
 vim.keymap.set("n", "<F12>", "<cmd>Dashboard<cr>")
 vim.keymap.set("n", "<tab><Right>", "<cmd>1bnext<cr>")
 vim.keymap.set("n", "<tab><Left>", "<cmd>1bprev<cr>")
+vim.keymap.set("n", "<tab>,", "<C-W>h")
+vim.keymap.set("n", "<tab>.", "<C-W>l")
+
+-- Extra Config
 vim.g.lazyvim_prettier_needs_config = false
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  command = [[%s/\s\+$//e]],
+})
+
+-- Custom Commands
+vim.api.nvim_create_user_command("Livegrep", "Telescope live_grep", {})
